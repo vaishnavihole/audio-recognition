3e272fb51491a8a548e4c47b7e9340a10ca14253
vaishnavihole <vaishnavihole1@gmail.com>
2023-11-08 12:13:40 +0530

    login form added

---
+++ client/src/views/Login/Login.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import './Login.css'; 
+import Footer from '../../component/Footer/Footer';
+import Navbar from '../../component/Navbar/Navbar';
+
+const LoginForm = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here, you can handle the form submission, e.g., send data to a server or perform client-side validation.
+    console.log(formData);
+  };
+
+  return (
+    <>
+    <Navbar />
+    <div className="login-form">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+    <Footer />
+    </>
+  );
+};
+
+export default LoginForm;
